d12967f6f21b77797f037a8f0b807f71fbdc32a4
guowangbulian <119483911+guowangbulian@users.noreply.github.com>
2022-12-16 11:06:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ plain='\033[0m'
 
 cur_dir=$(pwd)
 
+# check root
 [[ $EUID -ne 0 ]] && red "请在root用户下运行脚本" && exit 1
 
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
