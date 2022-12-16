dcec2309437549048843c8e65bc2eaac7d9efa62
guowangbulian <119483911+guowangbulian@users.noreply.github.com>
2022-12-16 15:11:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -133,7 +133,7 @@ download_xui(){
     chmod +x x-ui bin/xray-linux-$(archAffix)
     cp -f x-ui.service /etc/systemd/system/
     
-    wget -N --no-check-certificate https://raw.githubusercontent.com/guowangbulian/x-ui-1/edit/main/x-ui.sh -O /usr/bin/x-ui
+    wget -N --no-check-certificate https://raw.githubusercontent.com/guowangbulian/x-ui-1/main/x-ui.sh -O /usr/bin/x-ui
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
 }
